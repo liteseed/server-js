c9dbef28cdb72d21cc6c8fda70a6aedc11ac30e4
saklani <50768838+saklani@users.noreply.github.com>
2024-02-01 14:18:41 +0530

    fix database_url env var

--- src/services/database.ts
+++ src/services/database.ts
@@ -2,7 +2,7 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from '../schema';
 
-const client = neon(process.env.DRIZZLE_DATABASE_URL!);
+const client = neon(process.env.DATABASE_URL!);
 const database = drizzle(client, { schema });
 
 export default database
