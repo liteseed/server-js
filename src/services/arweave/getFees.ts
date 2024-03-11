51b2cd0f0c9ac836dff9b300c05fcaf3af1f28cc
saklani <50768838+saklani@users.noreply.github.com>
2024-03-11 11:49:00 +0530

    small fix

--- src/services/arweave/getFees.ts
+++ src/services/arweave/getFees.ts
@@ -1,7 +1,7 @@
 type GetFeesParams = { bytes: bigint };
 type GetFeesResponse = Promise<bigint>;
 
-export async function getFees(bytes: GetFeesParams): GetFeesResponse {
+export async function getFees({ bytes }: GetFeesParams): GetFeesResponse {
   const response = await fetch(`https://arweave.net/price/${bytes.toString()}`, {
     method: "GET",
   });
